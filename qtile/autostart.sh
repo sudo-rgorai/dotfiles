783287505725da04ea29ee7af4f363252734dfc8
sudo-rgorai <rahul.gorai.3141@gmail.com>
2021-11-22 21:29:55 +0530

    Add option for live wallpaper

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 picom &
-feh --bg-scale ~/Pictures/Wallpaper/silhoutte.jpg
+
+# Static Wallpaper
+feh --bg-scale ~/Pictures/Wallpaper/Wallpaper.png
+
+# Live Wallpaper
+# xwinwrap -ov -g 1366x768+0+0 -- mpv -wid WID ~/Pictures/Wallpaper/Wallpaper.mp4 --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-input-default-bindings &
